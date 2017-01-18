1a72f0b848a056180e1d2818c1b0122b7a27272a
LazarAngelov <nkcslxh@126.com>
2017-01-18 22:37:06 +0800

    start streaming finished

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
+# installation path of iptv streaming program, can be modified
+installPath='/usr/local/IPTVStreaming'
 
+# replace the absolute path with installation path
+sed -n "s/\/usr\/local\/IPTVStreaming/$installPath/g" iptvstreaming
+sed -n "s/\/usr\/local\/IPTVStreaming/$installPath/g" cfg/iptvstreaming.cfg
+sed -n "s/\/usr\/local\/IPTVStreaming/$installPath/g" lib/functions
+
+# for xmllint command
+yum -y install libxml2
+
+echo $0
