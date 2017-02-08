15a3b71343ecb98f717a13472a9f85836dce4cd4
LazarAngelov <nkcslxh@126.com>
2017-02-08 21:04:18 +0800

    use python to parse xml file

---
+++ lib/xmlParser.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import xml.etree.ElementTree as ET
+
+try:
+    tree = ET.parse(sys.argv[1])
+    root = tree.getroot()
+    
+    for stream in root.findall('Stream'):
+        name = stream.findtext('name')
+        source = stream.findtext('source')
+        cmd = 'echo '+source+' > '+sys.argv[2]+'/'+name
+        os.system(cmd)
+except:
+    sys.exit(1)
+
